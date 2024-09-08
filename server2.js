cff7a54fc3f644996b5737bf5ffb53475b9e2d80
Rushs321 <fyggh566@tutamail.com>
2024-09-08 22:07:18 +0530

    Update server2.js

--- server2.js
+++ server2.js
@@ -1,30 +1,11 @@
 #!/usr/bin/env node
 'use strict';
 
-import fastify from 'fastify';
-import { processRequest } from './src/proxy.js'; // Import the named export
-
-const app = fastify({ 
-  logger: false // Reduced logging level for performance
-          // Enable trust proxy for reverse proxies
-});
-
+const fastify = require('fastify');
+const { processRequest } = require('./src/proxy.js'); // Use require for named exports
+const app = fastify();
 const PORT = process.env.PORT || 8080;
 
-
-
-// Main route directly linked to processRequest
 app.get('/', processRequest);
 
-// Start the server
-const start = async () => {
-  try {
-    await app.listen({ host: '0.0.0.0', port: PORT });
-    console.log(`Server listening on port ${PORT}`);
-  } catch (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
-};
-
-start();
+app.listen({ host: '0.0.0.0', port: PORT })
