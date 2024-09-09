2179b323f10ef14898c4785aec302338c4cee110
Rushs321 <fyggh566@tutamail.com>
2024-09-09 08:54:17 +0530

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 "use strict";
+
 const userAgents = [
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
@@ -36,4 +37,7 @@ function randomUserAgent() {
   return userAgents[randomIndex];
 }
 
-module.exports = {generateRandomIP, randomUserAgent};
+module.exports = {
+  generateRandomIP,
+  randomUserAgent
+};
