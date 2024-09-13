a4c15fd8435009645956e7ebefa0615b2580b5f1
Tor-sudo <fhhhjgh567@tutamail.com>
2024-09-13 09:44:22 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-
-const fastify = require('fastify')();
-const processRequest = require('./src/proxy.js'); // Import the named export
-const PORT = process.env.PORT || 8080;
-
-fastify.get('/', processRequest);
-fastify.listen({host: '0.0.0.0', port: PORT });
