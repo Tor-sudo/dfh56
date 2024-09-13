802c1692aecb264709ed99f763e135d7001db791
Tor-sudo <fhhhjgh567@tutamail.com>
2024-09-13 10:57:14 +0530

    Update copy.js

--- src/copy.js
+++ src/copy.js
@@ -1,1 +1,1 @@
-"use strict";function copyHeaders(e,o){for(const[s,c]of Object.entries(e.headers)){try{o.header(s,c)}catch(err){console.log(err.message)}}}module.exports=copyHeaders;
+"use strict";function copy(e,o){for(const[s,c]of Object.entries(e.headers)){try{o.header(s,c)}catch(err){console.log(err.message)}}}module.exports=copy;
