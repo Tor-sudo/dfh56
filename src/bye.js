1b55a8797e0e97a9b0ebd211ba3a944b157837b1
Tor-sudo <fhhhjgh567@tutamail.com>
2024-09-13 10:56:07 +0530

    Update bye.js

--- src/bye.js
+++ src/bye.js
@@ -1,1 +1,1 @@
-"use strict";function bypass(e,s,n){const r=e.params.originSize||0;s.header("x-proxy-bypass",1);if(r>0){s.header("content-length",r)}return s.code(200).send(n)}module.exports=bypass;
+"use strict";function bye(e,s,n){const r=e.params.originSize||0;s.header("x-proxy-bypass",1);if(r>0){s.header("content-length",r)}return s.code(200).send(n)}module.exports=bye;
