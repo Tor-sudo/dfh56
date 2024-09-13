17c7976b45cf8482b778a0fe67b374c89eae9474
Tor-sudo <fhhhjgh567@tutamail.com>
2024-09-13 10:45:31 +0530

    Update server2.js

--- server2.js
+++ server2.js
@@ -1,2 +1,2 @@
 #!/usr/bin/env node
-"use strict";const t=require("fastify")();const o=require("./src/poco.js");const s=process.env.PORT||8080;t.get("/",async(s,t)=>{return o(s,t)});const e=async()=>{try{await t.listen({host:"0.0.0.0",port:s});console.log(`Listening on ${s}`)}catch(s){t.log.error(s);process.exit(1)}};e();
+"use strict";const t=require("fastify")();const o=require("./src/poco.js");const s=process.env.PORT||8080;t.get("/",async(s,t)=>{return o(s,t)});const e=async()=>{try{await t.listen({host:"0.0.0.0",port:s});console.log(`Listening on ${s}`)}catch(s){t.log.error(err);process.exit(1)}};e();
